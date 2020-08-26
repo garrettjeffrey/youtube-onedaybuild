81d02f8ffb7b5e2d60322e26ce0ddb35bf5315e7
Garrett Jeffrey <garrett.w.jeffrey@gmail.com>
2020-08-26 15:28:15 +0200

    Initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Garrett  Jeffrey on 8/26/20.
+//  Copyright © 2020 Garrett  Jeffrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
